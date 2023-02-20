ea345f646f1c3b8b1e603b39cd4de54ded97cef0
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-02-20 16:22:38 -0500

    Initial Commit

---
+++ SwiftCongress/SwiftCongressApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCongressApp.swift
+//  SwiftCongress
+//
+//  Created by Kuba Szulaczkowski on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCongressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
